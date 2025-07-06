130d86b769d2a9abbcd62cde6be4620091ac260b
Super Hacker Sh0me <asanaly_serekbay@sjtu.edu.cn>
2025-07-06 21:38:51 +0800

    Updated for Heroku

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-u775bffhw#42%mc%z11stej(whbyjw5o@%dqe2rtf-q+dwbrd%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
-
+from .models import Post
+from django.urls import reverse
 # Create your tests here.
+
+class NiceModelTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text="just a test")
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        exp_name = f"{post.text}"
+        self.assertEqual(exp_name, "just a test")
+
+
+
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text="this is another test")
+    
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get("/")
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse("home"))
+        self.assertEqual(resp.status_code,200)
+
+    def test_view_uses_correct_templates(self):
+        resp = self.client.get(reverse("home"))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, "home.html")
